792b8aaa46358da476d209aea8d9c8dd5fef1dd8
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-11-15 10:37:33 -0800

    for transfer

--- server.go
+++ server.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"path/filepath"
 	"time"
 
-	"github.com/txn2/n2proxy/sec"
+	"github.com/FredHutch/n2proxy/sec"
 
-	"github.com/txn2/n2proxy/rweng"
+	"github.com/FredHutch/n2proxy/rweng"
+	"github.com/gobuffalo/packr"
 	"go.uber.org/zap"
 )
 
@@ -104,8 +108,8 @@ func main() {
 	if skpverEnv == "true" {
 		skpverEnvBool = true
 	}
-	crtEnv := getEnv("CRT", "./example.crt")
-	keyEnv := getEnv("KEY", "./example.key")
+	// crtEnv := getEnv("CRT", "./example.crt")
+	// keyEnv := getEnv("KEY", "./example.key")
 
 	// command line falls back to env
 	port := flag.String("port", portEnv, "port to listen on.")
@@ -114,33 +118,37 @@ func main() {
 	backend := flag.String("backend", backendEnv, "backend server.")
 	logout := flag.String("logout", logoutEnv, "log output stdout | ")
 	srvtls := flag.Bool("tls", tlsEnvBool, "TLS Support (requires crt and key)")
-	crt := flag.String("crt", crtEnv, "Path to cert. (enable --tls)")
-	key := flag.String("key", keyEnv, "Path to private key. (enable --tls")
+	// crt := flag.String("crt", crtEnv, "Path to cert. (enable --tls)")
+	// key := flag.String("key", keyEnv, "Path to private key. (enable --tls")
 	skpver := flag.Bool("skip-verify", skpverEnvBool, "Skip backend tls verify.")
 	version := flag.Bool("version", false, "Display version.")
 	flag.Parse()
 
+	fmt.Println("dbg0")
 	if *version {
 		fmt.Printf("Version: %s\n", Version)
 		os.Exit(1)
 	}
 
-	zapCfg := zap.NewProductionConfig()
+	zapCfg := zap.NewDevelopmentConfig()
 	zapCfg.DisableCaller = true
 	zapCfg.DisableStacktrace = true
 	zapCfg.OutputPaths = []string{*logout}
+	fmt.Println("dbg1")
 
 	logger, err := zapCfg.Build()
 	if err != nil {
 		fmt.Printf("Can not build logger: %s\n", err.Error())
 		return
 	}
+	fmt.Println("dbg2")
 
 	err = logger.Sync()
 	if err != nil {
 		fmt.Printf("Error synchronizing logger: %s\n", err.Error())
 		os.Exit(1)
 	}
+	fmt.Println("dbg3")
 
 	logger.Info("Starting reverse proxy on port: " + *port)
 	logger.Info("Requests proxied to Backend: " + *backend)
@@ -167,6 +175,63 @@ func main() {
 		os.Exit(0)
 	}
 
+	dir, err := ioutil.TempDir("", "alpaca")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer fmt.Println("removed temp dir")
+	defer os.RemoveAll(dir)
+	defer fmt.Println("removing temp dir")
+	box := packr.NewBox("./certs")
+	crtEncoded, err := box.FindString("wildcard.fhcrc.org.crt.base64")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	crtDecoded, err := base64.StdEncoding.DecodeString(crtEncoded)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	crt := filepath.Join(dir, "crt.crt")
+	file, err := os.Create(crt)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	_, err = file.Write(crtDecoded)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	keyEncoded, err := box.FindString("fhcrc.org.key.base64")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	keyDecoded, err := base64.StdEncoding.DecodeString(keyEncoded)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	key := filepath.Join(dir, "key.key")
+	file2, err := os.Create(key)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file2.Close()
+	_, err = file2.Write(keyDecoded)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("key is", key)
+	fmt.Println("crt is", crt)
+
 	// Get a generic TLS configuration
 	tlsCfg := sec.GenericTLSConfig()
 
@@ -188,10 +253,32 @@ func main() {
 	srv.TLSConfig = tlsCfg
 	srv.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0)
 
-	err = srv.ListenAndServeTLS(*crt, *key)
+	err = srv.ListenAndServeTLS(crt, key)
 	if err != nil {
 		fmt.Printf("Error starting proxyin TLS mode: %s\n", err.Error())
 	}
+}
+
+func getKey() ([]byte, error) {
+	box := packr.NewBox("./certs")
+	return box.Find("fhcrc.org.key")
+}
+
+func getCrt() ([]byte, error) {
+	box := packr.NewBox("./certs")
+	return box.Find("wildcard.fhcrc.org.crtIIII")
+}
+
+func loadX509KeyPair() (tls.Certificate, error) {
+	certPEMBlock, err := getCrt()
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+	keyPEMBlock, err := getKey()
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+	return tls.X509KeyPair(certPEMBlock, keyPEMBlock)
 }
 
 // getEnv gets an environment variable or sets a default if
